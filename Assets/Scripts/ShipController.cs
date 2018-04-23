630bcd86adebf69aec6f52b555a1ebb82cb1d88c
ProProgrammerEric <eric_work@y7mail.com>
2018-04-23 11:29:06 +0930

    ShipController basics

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour {
 
+    public float BaseSpeed, SoftSpeedCap, HardSpeedCap, CurrentSpeed, BoostSpeedModifier;
+
+    private bool Boosted = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,32 @@ public class ShipController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        // Activate speed boost when either shift is held down
+        if (CurrentSpeed >= SoftSpeedCap &&
+            Input.GetKeyDown(KeyCode.LeftShift) ||
+            Input.GetKeyDown(KeyCode.RightShift))
+                Boosted = !Boosted;
+
+        // Speed up when W is held down
+        // Slow down when S is held down
+        CurrentSpeed += Input.GetAxis("Vertical") * BaseSpeed * Time.deltaTime;
+        CurrentSpeed = CurrentSpeed < BaseSpeed ? BaseSpeed : CurrentSpeed;
+
+        if (Boosted)
+        {
+            CurrentSpeed += BaseSpeed * 5 * Time.deltaTime;
+            if (CurrentSpeed > HardSpeedCap)
+                CurrentSpeed = HardSpeedCap;
+        }
+        else
+            if (CurrentSpeed > SoftSpeedCap)
+                CurrentSpeed = SoftSpeedCap;
+
 	}
+
+    void FixedUpdate()
+    {
+        transform.position += transform.forward * CurrentSpeed * Time.fixedDeltaTime;
+    }
 }
