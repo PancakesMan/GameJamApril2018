f94bc0706821464861d2a41b8d1c5d400e8fef22
ProProgrammerEric <eric_work@y7mail.com>
2018-04-24 14:59:17 +0930

    sound

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -51,7 +51,7 @@ public class ShipController : MonoBehaviour {
             foreach (var position in AttackPositions)
             {
                 Instantiate(Attack, position);
-                audioSources[0].PlayOneShot(audioSources[1].clip);
+                audioSources[1].PlayOneShot(audioSources[1].clip, 20);
             }
         }
 
