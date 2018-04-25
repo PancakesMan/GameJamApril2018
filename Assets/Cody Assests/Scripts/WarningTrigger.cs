ce0da1ead78749cfa5104492e29adc649bd8327e
CodyCammXD <cody.camm56@gmail.com>
2018-04-25 22:24:32 +0930

    ATTENTION! Description!
    The player can leave the field of battle we need to fix this, i have tried with a simple Trigger but this does not work because the location of the player is updated per frame? :trollface: If Eric sees this.... Any ideas? Thanks Cody.

---
+++ Assets/Cody Assests/Scripts/WarningTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningTrigger : MonoBehaviour {
+
+	public GameObject WarningText;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		WarningText.SetActive (false);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player")
+			WarningText.SetActive (true);
+	}
+}
+
